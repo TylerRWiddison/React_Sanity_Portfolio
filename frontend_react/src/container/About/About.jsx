25028b27f68b76f3a30e02785220e47c029fd2d4
Tyler Widdison <tyler.rwiddison@gmail.com>
2022-09-12 12:23:33 -0600

    Created About. Pulled data from sanity

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from "react";
+import { motion } from "framer-motion";
 
-import './About.scss';
+import { images } from "../../constants";
+import { urlFor, client } from "../../client";
+
+import "./About.scss";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  const [abouts, setAbouts] = useState([]);
 
-export default About
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
+  return (
+    <Fragment>
+      <h2 className="head-text">
+        A Good
+        <span> business </span>
+        <br />
+        needs a good
+        <span> website </span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </Fragment>
+  );
+};
+
+export default About;
