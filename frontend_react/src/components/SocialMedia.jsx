383cc6b4bd0da5b1ce670dc42844b2094d95732d
Tyler Widdison <tyler.rwiddison@gmail.com>
2022-09-14 16:59:18 -0600

    added functionality to social buttons

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import { BsLinkedin, BsTwitter } from 'react-icons/bs';
+import React from "react";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
 
 const SocialMedia = () => {
   return (
-    <div className='app__social'>
-      <div>
+    <div className="app__social">
+      <div
+        aria-label="linkedin.com/in/tyler-widdison"
+        onClick={() =>
+          window.open("https://www.linkedin.com/in/tyler-widdison")
+        }
+      >
         <BsLinkedin />
       </div>
-      <div>
-        <BsTwitter />
+      <div
+        aria-label="https://github.com/TylerRWiddison"
+        onClick={() => window.open("https://github.com/TylerRWiddison")}
+      >
+        <BsGithub />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SocialMedia
-
+export default SocialMedia;
